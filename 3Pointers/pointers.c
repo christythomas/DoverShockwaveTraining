898043318ea898bc2821b42e52ebb255bed5ece5
Christy Thomas <christy.thomas@gmail.com>
2016-11-21 18:06:28 -0800

    Update code and Makefiles

--- 3Pointers/pointers.c
+++ 3Pointers/pointers.c
@@ -1,27 +1,27 @@
 /* Source code to demonstrate, handling of pointers in C program */
 #include <stdio.h>
 
-int main(void)
-{
-    int *pc;
-    int c;
+int main(void) {
+	int *pc;
+	int c;
 
-    c = 22;
+	c = 22;
 
-    printf("\nAddress of c: %d\n", &c);
-    printf("Value of c: %d\n\n", c);
+	printf("\nAddress of c: 0x%08x\n", (unsigned int) &c);
+	printf("Value of c: %d\n\n", c);
 
-    pc = &c;
-    printf("Address of pointer pc: %d\n", pc);
-    printf("Content of pointer pc: %d\n\n", *pc);
+	pc = &c;
+	printf("Address of pointer pc: 0x%08x\n", (unsigned int) pc);
+	printf("Content of pointer pc: %d\n\n", *pc);
 
-    c = 11;
-    printf("Address of pointer pc: %d\n", pc);
-    printf("Content of pointer pc: %d\n\n", *pc);
+	c = 11;
+	printf("Address of pointer pc: 0x%08x\n", (unsigned int) pc);
 
-    *pc = 2;
-    printf("Address of c: %d\n", &c);
-    printf("Value of c: %d\n\n", c);
+	printf("Content of pointer pc: %d\n\n", *pc);
 
-    return 0;
+	*pc = 2;
+	printf("unsignedAddress of c: 0x%08x\n", (unsigned int) &c);
+	printf("Value of c: %d\n\n", c);
+
+	return 0;
 }
